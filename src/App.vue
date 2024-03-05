dfffeccf544d0cadab0814251d9a1c2ab9383a3c
Sergey87 <serge.techno@gmail.com>
2024-03-06 02:08:30 +0300

    added header - 1 part

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <script setup></script>
 
 <template>
-  <div class="text-slate-500"></div>
+  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
+    <header>
+      <div>
+        <img src="/logo.png" alt="logo" />
+        <div>
+          <h2>Vue Sneakers</h2>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+
+      <ul>
+        <li>
+          <img src="/cart.svg" alt="cart" />
+          <b>300 руб.</b>
+        </li>
+
+        <li>
+          <img src="/heart.svg" alt="heart" />
+          <b>Закладки</b>
+        </li>
+
+        <li>
+          <img src="/profile.svg" alt="profile" />
+          <b>Профиль</b>
+        </li>
+      </ul>
+    </header>
+  </div>
 </template>
 
 <style></style>
