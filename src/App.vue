a1a4508789127e97bbaeb57d596380a798067751
Sergey87 <serge.techno@gmail.com>
2024-03-06 23:54:09 +0300

    finished Header component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,10 @@
-<script setup></script>
+<script setup>
+import Header from './components/Header.vue'
+</script>
 
 <template>
   <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-10">
-    <header>
-      <div>
-        <img src="/logo.png" alt="logo" />
-        <div>
-          <h2>Vue Sneakers</h2>
-          <p>Магазин лучших кроссовок</p>
-        </div>
-      </div>
-
-      <ul>
-        <li>
-          <img src="/cart.svg" alt="cart" />
-          <b>300 руб.</b>
-        </li>
-
-        <li>
-          <img src="/heart.svg" alt="heart" />
-          <b>Закладки</b>
-        </li>
-
-        <li>
-          <img src="/profile.svg" alt="profile" />
-          <b>Профиль</b>
-        </li>
-      </ul>
-    </header>
+    <Header />
   </div>
 </template>
 
---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="flex justify-between border-b border-slate-300 px-10 py-8">
+    <div class="flex items-center gap-4">
+      <img src="/logo.png" alt="logo" class="w-10" />
+      <div>
+        <h2 class="text-xl font-bold uppercase">Vue Sneakers</h2>
+        <p class="text-slate-400">Магазин лучших кроссовок</p>
+      </div>
+    </div>
+
+    <ul class="flex items-center gap-10">
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/cart.svg" alt="cart" />
+        <b>300 руб.</b>
+      </li>
+
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/heart.svg" alt="heart" />
+        <span>Закладки</span>
+      </li>
+
+      <li class="flex items-center gap-3 cursor-pointer text-gray-500 hover:text-black">
+        <img src="/profile.svg" alt="profile" />
+        <span>Профиль</span>
+      </li>
+    </ul>
+  </header>
+</template>
