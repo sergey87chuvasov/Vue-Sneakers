87fd332ac2909e0f043a0fdb9d9822c6764be7b5
Sergey87 <serge.techno@gmail.com>
2024-03-08 23:02:26 +0300

    added Drawer component - part 2

---
+++ src/components/CartItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex items-center border border-slate-100 p-4 rounded-xl gap-4">
+    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+
+    <div class="flex flex-col">
+      <p>Кроссовки Nike Air Max 270</p>
+
+      <div class="flex justify-between mt-2">
+        <b>12800 руб.</b>
+        <img
+          class="opacity-40 hover:opacity-100 cursor-pointer transition"
+          src="/close.svg"
+          alt="close"
+        />
+      </div>
+    </div>
+  </div>
+</template>
--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,10 +1,13 @@
 <script setup>
-import DraverHead from './DraverHead.vue'
+import DrawerHead from './DrawerHead.vue'
+import CartItemList from './CartItemList.vue'
 </script>
 
 <template>
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
-    <DraverHead />
+    <DrawerHead />
+
+    <CartItemList />
   </div>
 </template>
