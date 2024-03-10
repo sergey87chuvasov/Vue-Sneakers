55cc13312220dbcc4c88e22ad159b81e43dc4b59
Sergey87 <serge.techno@gmail.com>
2024-03-10 16:56:35 +0300

    added search and select input

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,32 @@ import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
   <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-10">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевые)</option>
+            <option value="">По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
+            <input
+              type="text"
+              placeholder="Поиск"
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+            />
+          </div>
+        </div>
+      </div>
+
       <CardList />
     </div>
   </div>
