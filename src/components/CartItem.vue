e2cc949ddfcc7f5ee9256d200fa5bbd10feee4be
Sergey87 <serge.techno@gmail.com>
2024-04-05 22:19:47 +0300

    Development of shopping cart functionality - 2 part

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,13 +1,25 @@
+<script setup>
+const emit = defineEmits(['onClickRemove'])
+
+defineProps({
+  id: Number,
+  title: String,
+  imageUrl: String,
+  price: Number
+})
+</script>
+
 <template>
   <div class="flex items-center border border-slate-100 p-4 rounded-xl gap-4">
-    <img class="w-16 h-16" src="/sneakers/sneakers-1.jpg" alt="Sneaker" />
+    <img class="w-16 h-16" :src="imageUrl" :alt="title" />
 
-    <div class="flex flex-col">
-      <p>Кроссовки Nike Air Max 270</p>
+    <div class="flex flex-col flex-1">
+      <p>{{ title }}</p>
 
       <div class="flex justify-between mt-2">
-        <b>12800 руб.</b>
+        <b class="flex-1">{{ price }} BYN</b>
         <img
+          @click="emit('onClickRemove')"
           class="opacity-40 hover:opacity-100 cursor-pointer transition"
           src="/close.svg"
           alt="close"
