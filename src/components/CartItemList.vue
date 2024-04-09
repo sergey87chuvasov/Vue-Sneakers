b1f5a1db8571903b56ce711ab77f605a426cbd19
Sergey87 <serge.techno@gmail.com>
2024-04-09 22:35:47 +0300

    added auto animate library in project

--- src/components/CartItemList.vue
+++ src/components/CartItemList.vue
@@ -6,7 +6,7 @@ const { cart, removeFromCart } = inject('cart')
 </script>
 
 <template>
-  <div class="flex flex-col gap-4">
+  <div class="flex flex-col flex-1 gap-4 justify-between" v-auto-animate>
     <CartItem
       v-for="item in cart"
       :key="item.id"
